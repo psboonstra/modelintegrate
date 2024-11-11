390f6e8178e805bee1fbde1a2cfb07df901c91d1
psboonstra <philb@umich.edu>
2024-11-11 17:15:48 -0500

    more documentation

--- aux_functions/get_num_eff.R
+++ aux_functions/get_num_eff.R
@@ -1,4 +1,18 @@
-# function that starts with piecewise linear growth but becomes O(x^(1/3)) for x > 20
+#' Get prior number of non-zero parameters
+#' 
+#' Starts with piecewise linear growth but becomes O(x^(1/3)) for x > 20. So if
+#' you have a small number of variables, then you think that most of the
+#' corresponding parameters will be non-zero, but if you have a large number of
+#' variables, you think that most of the corresponding parameters will be zero.
+#'
+#' @param x An integer
+#'
+#' @return A number less than `x` that is used as the expected number of
+#'   non-zero parameters for prior elicitation purposes.
+#'
+#' @examples
+#' get_num_eff(3) # 2.85/3 = 95% non-zero
+#' get_num_eff(300) # 19.1/300 = 6% non-zero
 get_num_eff = function(x) {
   (x <= 3) * 0.95 * x + 
     (x > 3) * (x <= 10) * (3 * 0.95 + (x - 3) * 0.75) + 
--- aux_functions/log1plex.R
+++ aux_functions/log1plex.R
@@ -1,8 +1,15 @@
-# Safe function for accurately calculating log(1 + exp(x)) even for very large
-# and small x's
+#' Log1plex
+#' 
+#' Safe function for accurately calculating `log(1 + exp(x))` even for very
+#' large x's
+#'
+#' @param x A numeric vector
+#'
+#' @return `log(1 + exp(x))` for each element of x
+#'
+#' @examples
+#' log1plex(1e5) # correct answer
+#' log(1+exp(1e5)) # overflows
 log1plex = function(x) {
   - plogis(-x, log.p = TRUE)
-  #ifelse(x > 0,
-  #       x + log1p(exp(-x)),
-  #       log1p(exp(x)))
 }
